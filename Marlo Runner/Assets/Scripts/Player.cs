980f4eb6a2884d820c9f4cf26d57bfbd8bb8e7d3
Julian Sangillo <44471531+juliansangillo@users.noreply.github.com>
2020-02-29 17:10:40 -0500

    Added movement - part 1

--- Marlo Runner/Assets/Scripts/Player.cs
+++ Marlo Runner/Assets/Scripts/Player.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    public float movementSpeed = 4f;
+    public float acceleration = 2.5f;
+    public float jumpingSpeed = 6f;
+
+    private float speed = 0f;
+
     // Start is called before the first frame update
     void Start() {
         
@@ -11,7 +17,26 @@ public class Player : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        
+
+        speed += acceleration * Time.deltaTime;
+        if(speed > movementSpeed) {
+            speed = movementSpeed;
+        }
+
+        GetComponent<Rigidbody>().velocity = new Vector3(
+            speed,
+            GetComponent<Rigidbody>().velocity.y,
+            GetComponent<Rigidbody>().velocity.z
+        );
+
+        if(Input.GetMouseButtonDown(0) || Input.GetKey("space")) {
+            GetComponent<Rigidbody>().velocity = new Vector3(
+                GetComponent<Rigidbody>().velocity.x,
+                jumpingSpeed,
+                GetComponent<Rigidbody>().velocity.z
+            );
+        }
+
     }
 
 }

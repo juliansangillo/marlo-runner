a4626bdeb5742c069beb5b28970c66fef8bbbdc6
Julian Sangillo <44471531+juliansangillo@users.noreply.github.com>
2020-03-01 14:19:52 -0500

    Added more enemies - part 2

--- Marlo Runner/Assets/Scripts/Shell.cs
+++ Marlo Runner/Assets/Scripts/Shell.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Shell : MonoBehaviour {
 
     public float rotatingSpeed = 180f;
+    public float movementSpeed = 10f;
+
+    private bool movingRight = true;
     
     // Start is called before the first frame update
     void Start() {
@@ -16,6 +19,26 @@ public class Shell : MonoBehaviour {
         
         transform.RotateAround(transform.position, Vector3.up, rotatingSpeed * Time.deltaTime);
 
+        transform.position = new Vector3(
+            transform.position.x + movementSpeed * (movingRight ? 1 : -1) * Time.deltaTime,
+            transform.position.y,
+            transform.position.z
+        );
+
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if(other.GetComponent<Enemy>() != null) {
+            Destroy(other.gameObject);
+        }
+        else if(other.GetComponent<Brick>()) {
+            if(transform.position.x < other.transform.position.x && movingRight) {
+                movingRight = false;
+            }
+            else if(transform.position.x > other.transform.position.x && !movingRight) {
+                movingRight = true;
+            }
+        }
     }
 
 }

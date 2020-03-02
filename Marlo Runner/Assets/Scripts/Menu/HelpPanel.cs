578afa9fc8039e147bd1f6402547021bdbd5010d
Julian Sangillo <44471531+juliansangillo@users.noreply.github.com>
2020-03-02 13:28:24 -0500

    Added background and interface - part 3

---
+++ Marlo Runner/Assets/Scripts/Menu/HelpPanel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpPanel : MonoBehaviour {
+
+    public void OnConfirm() {
+        gameObject.SetActive(false);
+    }
+
+}
---
+++ Marlo Runner/Assets/Scripts/Menu/MenuController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+    public GameObject helpPanel;
+
+    public void OnPressPlay() {
+        SceneManager.LoadScene("Level1-1");
+    }
+
+    public void OnPressHelp() {
+        helpPanel.SetActive(true);
+    }
+
+}
